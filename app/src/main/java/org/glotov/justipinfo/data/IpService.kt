bb4a5c341a606a7fe5ca36600a5f00c8e686caec
Denis Glotov <denis@glotov.org>
2026-01-31 12:59:03 +0200

    add linting

--- app/src/main/java/org/glotov/justipinfo/data/IpService.kt
+++ app/src/main/java/org/glotov/justipinfo/data/IpService.kt
@@ -1,25 +1,25 @@
 package org.glotov.justipinfo.data
 
-import java.io.IOException
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import java.io.IOException
 
 class IpService {
-  private val client = OkHttpClient()
+    private val client = OkHttpClient()
 
-  fun fetchIpInfo(): String {
-    val request = Request.Builder().url("https://ipinfo.io/json").build()
+    fun fetchIpInfo(): String {
+        val request = Request.Builder().url("https://ipinfo.io/json").build()
 
-    return try {
-      client.newCall(request).execute().use { response ->
-        if (!response.isSuccessful) {
-          "Error: Code ${response.code} - ${response.message}"
-        } else {
-          response.body?.string() ?: "Error: Empty body"
+        return try {
+            client.newCall(request).execute().use { response ->
+                if (!response.isSuccessful) {
+                    "Error: Code ${response.code} - ${response.message}"
+                } else {
+                    response.body?.string() ?: "Error: Empty body"
+                }
+            }
+        } catch (e: IOException) {
+            "Error: ${e.message}"
         }
-      }
-    } catch (e: IOException) {
-      "Error: ${e.message}"
     }
-  }
 }
