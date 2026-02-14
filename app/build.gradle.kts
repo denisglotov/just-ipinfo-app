0071840663f14571ab7d96defc4aca0968ca5845
Denis Glotov <denis@glotov.org>
2026-02-14 23:41:20 +0200

    changelogs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "org.glotov.justipinfo"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
+        versionCode = 2
         versionName = "1.1"
         ndkVersion = "26.1.10909125"
 
